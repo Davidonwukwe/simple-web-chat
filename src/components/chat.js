ac21ef470f4a1c06ec1eaeff7688f0afe039ecdd
David Onwukwe <daveik55@gmail.com>
2022-09-21 14:31:30 +0100

    create main and chat components

---
+++ src/components/chat.js
@@ -0,0 +1,6 @@
+const Chat = () => {
+return (<div>
+    <h2>Enter your user name here</h2>
+</div>)
+}
+export default Chat;
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+import Chat from "./chat";
+
+const Main = () => {
+return(<div>
+
+    <h1>Simple Web Chat</h1>
+    <Chat/>
+</div>)
+}
+export default Main;
