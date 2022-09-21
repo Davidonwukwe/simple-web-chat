42eaeb7fe2b0702dccea7c1e697b6c1fbd414f7f
David Onwukwe <daveik55@gmail.com>
2022-09-21 14:45:16 +0100

    create input for user name and set values to local storage

--- src/components/main.js
+++ src/components/main.js
@@ -1,10 +1,46 @@
 import Chat from "./chat";
+import {useEffect, useState} from "react";
 
 const Main = () => {
-return(<div>
+    const [userName, setUserName] = useState(null);
+    const [currentUser, setCurrentUser] = useState(null);
 
-    <h1>Simple Web Chat</h1>
-    <Chat/>
-</div>)
+
+    const createCurrentUser = () => {
+        if (userName) {
+            const allUsersFromLocal = JSON.parse(localStorage.getItem('allUsersData'));
+            const currentUserFound = allUsersFromLocal ? allUsersFromLocal.find((user => user.name === userName)) : null
+            const tempAllUsers = allUsersFromLocal || [];
+            let tempCurrentUser;
+
+            if (currentUserFound) {
+                tempCurrentUser = currentUserFound
+            } else {
+                tempCurrentUser = {
+                    name: userName,
+
+                }
+                tempAllUsers.push(tempCurrentUser);
+            }
+            window.sessionStorage.setItem("currentUserData", JSON.stringify(tempCurrentUser));
+            window.localStorage.setItem("allUsersData", JSON.stringify(tempAllUsers));
+        }
+
+    }
+    return(
+        <div>
+            <h1>Simple Web Chat</h1>
+
+            <div>
+                {!currentUser && <div>
+                    <label htmlFor="">Enter your username</label>
+                    <input className="form-control my-1" onChange={(event) => setUserName(event.target.value)}  type="text"/>
+                    <button className="btn btn-primary w-100 my-2" onClick={createCurrentUser} >Login</button>
+
+                </div>}
+            </div>
+
+            <Chat/>
+        </div>)
 }
 export default Main;
